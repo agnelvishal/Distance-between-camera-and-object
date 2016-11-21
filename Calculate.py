7c3f92b4c8c8b66642892e6a28eaf1dd5a72f1e2
Agnel Vishal <agnelvishal@gmail.com>
2016-11-21 13:17:52 +0530

    Update Calculate.py

--- Calculate.py
+++ Calculate.py
@@ -1,10 +1,10 @@
 import math
-from imutils import perspective
 import numpy as np
-import imutils
 import cv2
 import time
 from matplotlib import pyplot as plt
+import imutils
+from imutils import perspective
 
 def getmp(image):
     
@@ -13,34 +13,25 @@ def getmp(image):
     gray = cv2.GaussianBlur(gray, (7,7), 0)
     gray = cv2.GaussianBlur(gray, (7,7), 0)
     edged = cv2.inRange(gray, 0, 70)
-    
-    
-
-    
     edged = cv2.dilate(edged, None, iterations=10)
-    
     edged = cv2.erode(edged, None, iterations=5)
-    
     edged = cv2.dilate(edged, None, iterations=10)
-    
     edged = cv2.erode(edged, None, iterations=30)
     edged = cv2.dilate(edged, None, iterations=40)
     ff=np.where(edged>50)
-    a1=np.mean(ff[1])
-
-    
+    a1=np.mean(ff[1])  
     return a1
+
 lo=cv2.imread('a30.jpg')
 ro=cv2.imread('b30.jpg')
-
-
-#lr=getmp(ro)
 lp=getmp(lo)
 lr=getmp(ro)
+p=abs(lp-lr)
+with open("DoNotDelete.txt",'r') as g:
+    dpp=g.read()
+    dpp=float(dpp)
+    print dpp*p
 
-pdi=abs(lp-lr)
-y=(pdi*(-8.1167))+28875.91
-print y/pdi
 
 
       
